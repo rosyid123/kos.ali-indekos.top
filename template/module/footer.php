c28f96b53af4e8d2d794817e0f20cb8e3a55b237
rosyid123 <rosyidkarim@gmail.com>
2017-01-22 23:42:13 +0700

    Add files via upload

---
+++ template/module/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 1.0.0 Exclusive
+        </div>
+        <strong>Copyright &copy; 2016 <a href="#">Amin-Bagus-Ulil</a>.</strong> All rights reserved.
+      </footer>
